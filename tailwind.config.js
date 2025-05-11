9cc32cb222ece07a01e549ab5f2cda3b4430344a
purv-kabaria <153810916+OldmanGalaxy@users.noreply.github.com>
2025-05-12 04:36:42 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#EEEEEE',
+        secondary: '#ed1c24',
+        accent: '#14284b',
+      },
+      fontFamily: {
+        cal: ['Cal-Sans', 'sans-serif'],
+        mont: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} 
